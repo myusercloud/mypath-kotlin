4afe10a3fb5cc633ecf295b54b3793e44221d7b3
myusercloud <lucasharrizon@gmail.com>
2025-03-26 10:22:22 +0300

    added files: constructors

---
+++ src/constructors.kt
@@ -0,0 +1,13 @@
+class dog(var name: String, var breed: String, var age: Int){
+
+}
+
+fun main() {
+    var dog1 = dog("Tito", "Chihuahua", 5)
+    var dog2 = dog("Jerry", "Pitbull", 2)
+    var dog3 = dog("Tina", "Siberian Husky", 6)
+    var dog4 = dog("Perpetual", "German shepherd", 1)
+
+    println(dog1.name)
+
+}
